b9943d91a9572ea2fd055a5a205b624c9f04302a
Ely <cakeislie.ilya@gmail.com>
2021-07-08 02:05:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = 'playtxt',
     version = '1.0',
     description = 'YouTube audio stream player...that can only play songs from a text file with song names',
-    author = 'JetFly',
+    author = 'ThatMightyGuy',
     author_email = 'cakeislie.ilya@gmail.com',
     scripts = [
         'scripts/playtxt.py'
