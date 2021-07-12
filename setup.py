4651de6934c82a59665cf47eaadfb9ee9cccf98b
Ely <cakeislie.ilya@gmail.com>
2021-07-12 22:06:43 +0300

    Add files via upload
    Added the "install_requires" list

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 setup(
     name = 'playtxt',
-    version = '1.0',
+    version = '1.11',
     description = 'YouTube audio stream player...that can only play songs from a text file with song names',
-    author = 'ThatMightyGuy',
+    author = 'Ilya (ThatMightyGuy, JetFly) Vitsev',
     author_email = 'cakeislie.ilya@gmail.com',
     scripts = [
         'scripts/playtxt.py'
+    ],
+    install_requires = [
+        'bs4',
+        'requests'
     ],
     data_files = [
         ('~/.playtxt', [])
